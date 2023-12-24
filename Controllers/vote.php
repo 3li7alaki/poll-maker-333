18dd413215cc0d7068c46ad54e8121296f94a1e1
Ali AlHalaki <AliHalaki@outlook.com>
2023-12-24 12:50:15 +0300

    Fix

--- Controllers/vote.php
+++ Controllers/vote.php
@@ -1,12 +1,17 @@
 <?php
 
+use Models\Poll;
+
 require 'dependencies.php';
 require 'loggedIn.php';
 
 $user = $_SESSION['user'];
 
 if (isset($_POST['option_id']) && isset($_POST['poll_id'])) {
-    if (!$user->voted($_POST['poll_id'])) {
+    $poll = Poll::find($_POST['poll_id']);
+    $expired = !is_null($poll->end_date) && strtotime($poll->end_date) < time();
+
+    if (!$user->voted($_POST['poll_id']) && !$expired) {
         $voted = $user->vote($_POST['poll_id'], $_POST['option_id']);
     }
     header('Location: ../Views/viewPoll.php?pollId='.$_POST['poll_id']);
--- pollmaker.sql
+++ pollmaker.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 22, 2023 at 12:22 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 24, 2023 at 09:25 AM
+-- Server version: 10.6.12-MariaDB-cll-lve
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pollmaker`
+-- Database: `u515027391_pollmaker`
 --
 
 -- --------------------------------------------------------
@@ -38,57 +38,47 @@ CREATE TABLE `options` (
 --
 
 INSERT INTO `options` (`id`, `option_text`, `poll_id`) VALUES
-(4, 'Option 1', 7),
-(5, 'Option 2', 7),
-(6, 'Option 3', 7),
-(7, 'Option 1', 8),
-(8, 'Option 12', 8),
-(9, '333', 8),
-(10, 'Option1', 9),
-(11, 'option2', 9),
-(12, 'Option3', 9),
-(13, 'Bread', 10),
-(14, 'Toast', 10),
-(15, 'cristiano ronaldo', 11),
-(16, 'leo messi', 11),
-(17, 'neymar jr', 11),
-(18, 'Action', 12),
-(19, 'Comedy', 12),
-(20, 'Drama', 12),
-(21, 'Sci-Fi', 12),
-(22, 'Horror', 12),
-(23, 'Spring', 13),
-(24, 'Summer', 13),
-(25, 'Autumn/Fall', 13),
-(26, 'Winter', 13),
-(27, 'Black', 14),
-(28, 'With cream', 14),
-(29, 'With sugar', 14),
-(30, 'Latte', 14),
-(31, 'I don&#039;t drink coffee', 14),
-(32, 'Facebook', 15),
-(33, 'Instagram', 15),
-(34, 'Twitter', 15),
-(35, 'TikTok', 15),
-(36, 'Chips', 16),
-(37, 'Fruit', 16),
-(38, 'Chocolate', 16),
-(39, 'Nuts', 16),
-(40, 'Popcorn', 16),
-(41, 'Fiction', 17),
-(42, 'Non-fiction', 17),
-(43, 'Mystery/Thriller', 17),
-(44, 'Fantasy', 17),
-(45, 'Romance', 17),
-(46, 'Italian', 18),
-(47, 'Asian', 18),
-(48, 'Mexican', 18),
-(49, 'Mediterranean', 18),
-(50, 'PC', 19),
-(51, 'Xbox', 19),
-(52, 'PlayStation', 19),
-(53, 'Nintendo', 19),
-(54, 'Mobile gaming', 19);
+(1, 'Yes, I have watched many', 1),
+(2, 'Yes, only a few', 1),
+(3, 'Yes, only one', 1),
+(4, 'No I haven&#039;t', 1),
+(9, 'Yes', 3),
+(10, 'No', 3),
+(11, 'Maybe', 3),
+(12, 'I am not sure', 3),
+(13, 'Yes', 4),
+(14, 'No', 4),
+(15, 'London', 5),
+(16, 'Paris', 5),
+(17, 'New York', 5),
+(18, 'Leonel Messi', 6),
+(19, 'Cristiano ronaldo', 6),
+(20, 'Karim Benzema ', 6),
+(21, 'Yes', 7),
+(22, 'No', 7),
+(23, 'Partially', 7),
+(24, 'Not sure', 7),
+(25, 'Qatar ', 8),
+(26, 'Egypt ', 8),
+(27, 'Italy', 8),
+(28, 'Spain', 8),
+(29, 'Yes', 9),
+(30, 'No', 9),
+(31, 'To some extent', 9),
+(32, 'Not sure', 9),
+(33, 'Cognitive Psychology', 10),
+(34, 'Social Psychology', 10),
+(35, 'Behavioral Psychology', 10),
+(36, 'Clinical Psychology', 10),
+(37, 'Yes', 11),
+(38, 'No', 11),
+(39, 'Never used the method', 11),
+(40, 'League of Legends', 12),
+(41, 'Dota 2', 12),
+(42, 'Counter-Strike: Global Offensive', 12),
+(43, 'Overwatch', 12),
+(47, 'Micheal John', 14),
+(48, 'Harold mark', 14);
 
 -- --------------------------------------------------------
 
@@ -111,19 +101,18 @@ CREATE TABLE `polls` (
 --
 
 INSERT INTO `polls` (`id`, `title`, `end_date`, `user_id`, `category`, `time_created`, `time_updated`) VALUES
-(7, 'What is your favorite bread????', '2024-01-06 12:00:00', 1, 'helloo', '2023-12-13 09:05:14', '2023-12-13 13:58:05'),
-(8, 'Test Title', NULL, 1, 'test2', '2023-12-13 12:50:52', '2023-12-13 14:02:26'),
-(9, 'Title 123', '2023-12-26 16:01:00', 1, 'Category123', '2023-12-13 13:02:02', '2023-12-13 13:02:02'),
-(10, 'What is toast?', NULL, 1, 'Bread', '2023-12-13 14:03:44', '2023-12-13 14:03:44'),
-(11, 'who&#039;s your favorite football player?', NULL, 1, 'sports', '2023-12-21 22:38:01', '2023-12-21 22:38:01'),
-(12, 'Which genre of movies do you enjoy the most?', '2023-12-28 01:43:00', 2, 'entertainment', '2023-12-21 22:44:02', '2023-12-21 22:44:02'),
-(13, 'What&#039;s your favorite season?', NULL, 2, 'personal', '2023-12-21 22:45:11', '2023-12-21 22:45:11'),
-(14, 'How do you like your coffee?', NULL, 4, 'personal', '2023-12-21 22:57:47', '2023-12-21 22:57:47'),
-(15, 'Which social media platform do you use the most?', '2023-12-30 01:58:00', 4, 'entertainment', '2023-12-21 22:58:32', '2023-12-21 22:58:32'),
-(16, 'What&#039;s your go-to snack?', '2024-02-22 02:01:00', 5, 'food', '2023-12-21 23:02:07', '2023-12-21 23:02:07'),
-(17, 'Which type of books do you enjoy reading?', NULL, 5, 'educational', '2023-12-21 23:04:27', '2023-12-21 23:04:27'),
-(18, 'What&#039;s your favorite type of cuisine?', '2024-02-07 02:13:00', 6, 'food', '2023-12-21 23:13:21', '2023-12-21 23:13:21'),
-(19, 'Which gaming platform do you prefer?', NULL, 7, 'entertainment', '2023-12-21 23:17:31', '2023-12-21 23:17:31');
+(1, 'Have you ever watched an Esports tournament or match?', NULL, 2, 'Esports', '2023-12-23 09:09:50', '2023-12-23 09:09:50'),
+(3, 'Is sadness the same as depression?', '2023-12-31 14:29:00', 4, 'Human Science', '2023-12-23 11:31:31', '2023-12-23 11:31:31'),
+(4, 'Is race a social construct?', '2024-01-23 20:27:00', 5, 'Human Science', '2023-12-23 17:28:34', '2023-12-23 17:28:34'),
+(5, 'What is your favorite travel destination', '2023-12-29 20:34:00', 6, 'Travel', '2023-12-23 17:34:48', '2023-12-23 17:34:48'),
+(6, 'Who is your favorite football player ', '2023-12-24 11:14:58', 8, 'Esports', '2023-12-23 17:37:03', '2023-12-24 08:14:58'),
+(7, 'Do you think AI will take over the jobs of software engineers?', NULL, 7, 'Science and Technology', '2023-12-23 17:39:45', '2023-12-23 17:39:45'),
+(8, 'Which country is best to travel to?', NULL, 9, 'Travel', '2023-12-23 18:11:32', '2023-12-23 18:11:32'),
+(9, 'Should universities allow the use of AI academically?', NULL, 7, 'Science and Technology', '2023-12-23 18:21:42', '2023-12-23 18:21:42'),
+(10, 'What aspect of psychology interests you the most?', NULL, 10, 'Human Science', '2023-12-23 18:25:17', '2023-12-23 18:25:17'),
+(11, 'Is the Pomodoro method an effective way to study?', '2023-12-28 21:32:00', 13, 'Human Science', '2023-12-23 18:32:51', '2023-12-23 18:32:51'),
+(12, 'Which esports game is your favorite?', NULL, 12, 'Esports', '2023-12-23 18:33:51', '2023-12-23 18:33:51'),
+(14, 'Who is your favorite Basket player ', NULL, 8, 'Esports', '2023-12-24 08:19:14', '2023-12-24 08:19:14');
 
 -- --------------------------------------------------------
 
@@ -146,13 +135,26 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `is_admin`, `time_created`, `time_updated`) VALUES
-(1, 'Ali AlHalaki', 'Ali@outlook.com', '$2y$10$iMerBNsOnlIQkeq9Nm14t.tE35KS4uC2kRgTuZy7YevSRPDlUaYRC', NULL, '2023-12-11 10:42:22', '2023-12-13 12:54:56'),
-(2, 'Abdullah', 'Abdullah@email.com', '$2y$10$XMqYMw0yP02NTrZeNx0ufOmJAlfIxOO5saY300LoENnGPCmWiks0u', NULL, '2023-12-11 15:39:03', '2023-12-11 15:39:03'),
-(3, 'Ayoub', 'Ayoub@weed.com', '$2y$10$wuZFXiFYmym5.zTchLbXZekZiVHPMZ0ai0VFc5o9ohF6z6PVHNAWu', NULL, '2023-12-11 16:21:08', '2023-12-11 16:21:08'),
-(4, 'mohammed', 'mohammed@gmail.com', '$2y$10$.zE91ZNiWEKpMlvHqRa1PeNOZ5DRRQ7dE8DUXrNO1cpRbOikFVmE2', NULL, '2023-12-21 22:56:00', '2023-12-21 22:56:00'),
-(5, 'hasan', 'hasan@yahoo.com', '$2y$10$0j9pv9xogT2TZth/xmPnyucnv5zbtWhgGEMgWRaxQcO81HY2lpokK', NULL, '2023-12-21 23:00:11', '2023-12-21 23:00:11'),
-(6, 'yaseen', 'yaseen@stu.uob.edu.bh', '$2y$10$YWquA5qDJCfk5hlZCJip8e/g40fQ3N9hvrdF7reSvtwOI6xLtmoRm', NULL, '2023-12-21 23:11:47', '2023-12-21 23:14:35'),
-(7, 'nasser', 'nasser@hotmail.com', '$2y$10$di6EMlzla2SBhM9FNvTJveiCfoChkrEzv/li0hrote485IuHYJnhS', NULL, '2023-12-21 23:16:05', '2023-12-21 23:16:05');
+(1, 'Admin', 'Admin@Poll.com', '$2y$10$cKHMhYhYRn1vh0m/ISk.P.wz5JMtfVq54OJSx.Dv2ppK.YoEqfDX6', 1, '2023-12-22 19:15:04', '2023-12-23 08:47:23'),
+(2, 'Abdulrahman Mahaini', 'abdulrahmanmahainy@gmail.com', '$2y$10$kgyMlPhgY998znPsdhzYT.8uln20FRVDjta3fIImSpZgm5atp9GQ2', NULL, '2023-12-23 09:07:41', '2023-12-23 09:07:41'),
+(3, 'John Micheal', 'bombaclat123@gmail.com', '$2y$10$yLNWkeE0S7zCB2Mr.JwcNeqk5alKIPuvUkshcaLRJOinedoVWP43.', NULL, '2023-12-23 11:27:40', '2023-12-23 11:33:25'),
+(4, 'AR', 'lol@gmail.com', '$2y$10$B0IpMSlatVL6tXDrcR.wPuO4LofWZhCzQtCkFDFhvi2Uf3EYE7McW', NULL, '2023-12-23 11:28:24', '2023-12-23 11:28:24'),
+(5, 'Seld Aniga', 'maher3f@hotmail.com', '$2y$10$yr9lsuA1V66GF/ZArSdTg.aARRIx0mFCjSVwhdLaZMnFvOyCq.dpa', NULL, '2023-12-23 12:00:14', '2023-12-23 12:00:14'),
+(6, 'Ali sami', 'Ali@poll.com', '$2y$10$xqrPmaI.zjQSIoq.Te5lbeBBG2QqNoA1qLi/qwhc8snXOhDyx3J7e', NULL, '2023-12-23 17:30:58', '2023-12-23 17:30:58'),
+(7, 'Ayoub Fouad', 'Ayoub@Poll.com', '$2y$10$udm3D/jh2Z555VGWQeFKe..MbaJeVhyuDJYqdE9nINiIP4wdtIEBi', NULL, '2023-12-23 17:33:23', '2023-12-23 17:33:23'),
+(8, 'Hussain sami', 'Hussain@poll.com', '$2y$10$FDS4I4HR/ybPl/lxLOj85OuGDNVpn/dQdH0BuVEFrkANgKpzj3YlO', NULL, '2023-12-23 17:35:28', '2023-12-23 17:35:28'),
+(9, 'Abdulla Ali', 'Abdulla@poll.com', '$2y$10$p/8zqrpmR3sekv1u2f3kWuEGTeAPyut0KGceYt9.ATGHk.NpgKvfu', NULL, '2023-12-23 18:08:29', '2023-12-23 18:08:29'),
+(10, 'James Barnes', 'James@poll.com', '$2y$10$oRI7U71aqkZOaiOAn559s..lwqjTgvmahrbNt/XP3y9JuZzOvlFNm', NULL, '2023-12-23 18:20:45', '2023-12-23 18:20:45'),
+(11, 'Nasser', 'Nasser@Poll.com', '$2y$10$k88D5Iz5YKHaeg29J5ZTKuXFGBcac6Bkwfh6rAakMnODFE3NK26L6', NULL, '2023-12-23 18:25:47', '2023-12-23 18:25:47'),
+(12, 'Micheal Jackson', 'Micheal@poll.com', '$2y$10$cOW6LvdS706M4SrDSHG8He9o3zHg04HeUZqZamr9rblmbIlKfjrNe', NULL, '2023-12-23 18:29:25', '2023-12-23 18:29:25'),
+(13, 'Yassen', 'Yassen@Poll.com', '$2y$10$jr540X.raxXG8RkVc1lnHeMiwYn4mMcH.Et5OKbLGd1Llz4zQHR1y', NULL, '2023-12-23 18:31:21', '2023-12-23 18:31:21'),
+(14, 'Jaffar Ali', 'fchgvjbkh@gmail.com', '$2y$10$zpeYhX9SO0yRgfPJ7hLNf.3jwOdIfaGvF0qKIme6p03zeS6mFtOxK', NULL, '2023-12-23 18:35:41', '2023-12-23 18:35:41'),
+(15, 'Peter Griffin ', 'Peter@poll.com', '$2y$10$b4o/MCwSnYw5UX2cD2FbcO4OK1M/c0Z95ctZTP7JVHYY2Ueim/q3K', NULL, '2023-12-23 18:38:34', '2023-12-23 18:38:34'),
+(16, 'Emily Johnson', 'Emily@poll.com', '$2y$10$.NCGoW62a1PDIwHKb47O2uiLrelcxcrESdI.aDM6Nb.BrWC/eSqJy', NULL, '2023-12-23 18:43:52', '2023-12-23 18:43:52'),
+(17, 'Jasmine Clark', 'Jasmine@poll.com', '$2y$10$XgGrZWHgKHKOss/CHg305O0MSLkDDvkVHq4YPXiFe9/HxjyCKYjSa', NULL, '2023-12-23 18:47:52', '2023-12-23 18:47:52'),
+(18, 'Ethan Williams', 'Ethan@poll.com', '$2y$10$XYKGAI/zbUKkySdN0Agceu3655AXGw1enNaE1RSVjcxBEqcCWpN9O', NULL, '2023-12-23 18:50:58', '2023-12-23 18:50:58'),
+(19, 'Chloe Anderson', 'Chloe@poll.com', '$2y$10$5VM1vhNal6C8GUGq/mHBqO9vLRGE6ZPPLXWhk0dE0YAzO.DEJqArW', NULL, '2023-12-23 18:53:48', '2023-12-23 18:53:48'),
+(20, 'Rashid Khalaf', 'Rashid@poll.com', '$2y$10$4Dh9K4qK0FJb52QqlcoMAe07mc9bAn03cwpahlZgbTxWPe6eGajiW', NULL, '2023-12-23 19:50:03', '2023-12-23 19:50:03');
 
 -- --------------------------------------------------------
 
@@ -173,9 +175,140 @@ CREATE TABLE `votes` (
 --
 
 INSERT INTO `votes` (`id`, `user_id`, `poll_id`, `option_id`, `vote_date`) VALUES
-(1, 2, 7, 4, '2023-12-13 13:39:26'),
-(2, 3, 7, 5, '2023-12-13 13:39:51'),
-(3, 3, 7, 4, '2023-12-13 13:42:38');
+(1, 1, 1, 2, '2023-12-23 09:10:19'),
+(2, 4, 1, 3, '2023-12-23 11:31:53'),
+(3, 5, 1, 2, '2023-12-23 12:00:55'),
+(4, 5, 3, 10, '2023-12-23 12:01:52'),
+(5, 1, 4, 13, '2023-12-23 17:29:03'),
+(6, 6, 1, 4, '2023-12-23 17:31:14'),
+(7, 6, 3, 10, '2023-12-23 17:31:28'),
+(8, 6, 4, 13, '2023-12-23 17:31:35'),
+(9, 8, 4, 14, '2023-12-23 17:35:35'),
+(10, 8, 1, 3, '2023-12-23 17:35:42'),
+(11, 7, 3, 12, '2023-12-23 17:35:48'),
+(12, 8, 5, 17, '2023-12-23 17:37:11'),
+(13, 5, 4, 14, '2023-12-23 17:37:26'),
+(14, 1, 6, 19, '2023-12-23 17:46:06'),
+(15, 1, 7, 23, '2023-12-23 17:46:28'),
+(16, 9, 1, 2, '2023-12-23 18:14:12'),
+(17, 9, 3, 11, '2023-12-23 18:14:45'),
+(18, 9, 4, 13, '2023-12-23 18:14:59'),
+(19, 9, 5, 17, '2023-12-23 18:15:17'),
+(20, 9, 6, 20, '2023-12-23 18:15:28'),
+(21, 9, 7, 22, '2023-12-23 18:15:41'),
+(22, 9, 8, 25, '2023-12-23 18:15:57'),
+(23, 10, 1, 1, '2023-12-23 18:20:52'),
+(24, 10, 3, 9, '2023-12-23 18:21:02'),
+(25, 10, 4, 13, '2023-12-23 18:21:11'),
+(26, 10, 5, 15, '2023-12-23 18:21:17'),
+(27, 10, 6, 18, '2023-12-23 18:21:31'),
+(28, 10, 7, 21, '2023-12-23 18:21:39'),
+(29, 10, 8, 28, '2023-12-23 18:21:46'),
+(30, 7, 8, 26, '2023-12-23 18:22:51'),
+(31, 10, 10, 34, '2023-12-23 18:25:24'),
+(32, 10, 9, 31, '2023-12-23 18:25:46'),
+(33, 11, 3, 11, '2023-12-23 18:25:55'),
+(34, 11, 5, 17, '2023-12-23 18:26:05'),
+(35, 9, 9, 29, '2023-12-23 18:26:34'),
+(36, 11, 10, 33, '2023-12-23 18:26:36'),
+(37, 9, 10, 34, '2023-12-23 18:26:52'),
+(38, 11, 8, 25, '2023-12-23 18:26:55'),
+(39, 11, 9, 31, '2023-12-23 18:28:41'),
+(40, 12, 1, 2, '2023-12-23 18:30:45'),
+(41, 12, 3, 10, '2023-12-23 18:30:54'),
+(42, 12, 4, 13, '2023-12-23 18:31:01'),
+(43, 12, 5, 16, '2023-12-23 18:31:13'),
+(44, 12, 6, 19, '2023-12-23 18:31:21'),
+(45, 12, 7, 22, '2023-12-23 18:31:30'),
+(46, 12, 8, 25, '2023-12-23 18:31:38'),
+(47, 12, 9, 32, '2023-12-23 18:31:50'),
+(48, 12, 10, 35, '2023-12-23 18:31:58'),
+(49, 13, 9, 31, '2023-12-23 18:33:03'),
+(50, 13, 6, 18, '2023-12-23 18:33:12'),
+(51, 13, 10, 34, '2023-12-23 18:33:26'),
+(52, 12, 11, 37, '2023-12-23 18:34:00'),
+(53, 12, 12, 42, '2023-12-23 18:34:12'),
+(54, 9, 12, 43, '2023-12-23 18:34:39'),
+(55, 9, 11, 37, '2023-12-23 18:34:46'),
+(56, 10, 12, 42, '2023-12-23 18:35:14'),
+(57, 10, 11, 37, '2023-12-23 18:35:21'),
+(58, 14, 1, 2, '2023-12-23 18:36:38'),
+(59, 14, 3, 10, '2023-12-23 18:36:59'),
+(60, 14, 4, 13, '2023-12-23 18:37:15'),
+(61, 14, 5, 15, '2023-12-23 18:37:35'),
+(62, 14, 6, 19, '2023-12-23 18:37:49'),
+(63, 14, 7, 23, '2023-12-23 18:38:00'),
+(64, 14, 8, 27, '2023-12-23 18:38:12'),
+(65, 14, 9, 31, '2023-12-23 18:38:23'),
+(66, 14, 10, 36, '2023-12-23 18:38:38'),
+(67, 15, 1, 1, '2023-12-23 18:38:43'),
+(68, 14, 11, 39, '2023-12-23 18:38:49'),
+(69, 14, 12, 41, '2023-12-23 18:39:03'),
+(70, 15, 3, 11, '2023-12-23 18:41:03'),
+(71, 15, 4, 14, '2023-12-23 18:41:09'),
+(72, 15, 5, 15, '2023-12-23 18:41:14'),
+(73, 15, 6, 19, '2023-12-23 18:41:19'),
+(74, 15, 7, 23, '2023-12-23 18:41:29'),
+(75, 15, 8, 25, '2023-12-23 18:41:36'),
+(76, 15, 9, 30, '2023-12-23 18:41:56'),
+(77, 15, 10, 34, '2023-12-23 18:42:03'),
+(78, 15, 11, 37, '2023-12-23 18:42:12'),
+(79, 15, 12, 42, '2023-12-23 18:42:24'),
+(80, 16, 1, 2, '2023-12-23 18:44:51'),
+(81, 16, 3, 12, '2023-12-23 18:44:55'),
+(82, 16, 4, 13, '2023-12-23 18:45:00'),
+(83, 16, 5, 15, '2023-12-23 18:45:06'),
+(84, 16, 6, 19, '2023-12-23 18:45:12'),
+(85, 16, 7, 24, '2023-12-23 18:45:20'),
+(86, 16, 8, 28, '2023-12-23 18:45:30'),
+(87, 16, 9, 29, '2023-12-23 18:45:44'),
+(88, 16, 10, 35, '2023-12-23 18:45:55'),
+(89, 16, 11, 39, '2023-12-23 18:46:07'),
+(90, 16, 12, 40, '2023-12-23 18:46:15'),
+(91, 17, 1, 2, '2023-12-23 18:47:57'),
+(92, 17, 3, 10, '2023-12-23 18:48:08'),
+(93, 17, 4, 13, '2023-12-23 18:48:15'),
+(94, 17, 5, 17, '2023-12-23 18:48:24'),
+(95, 17, 6, 20, '2023-12-23 18:48:32'),
+(96, 17, 7, 23, '2023-12-23 18:48:41'),
+(97, 17, 8, 27, '2023-12-23 18:48:51'),
+(98, 17, 9, 31, '2023-12-23 18:49:09'),
+(99, 17, 10, 34, '2023-12-23 18:49:20'),
+(100, 17, 11, 37, '2023-12-23 18:49:27'),
+(101, 17, 12, 43, '2023-12-23 18:49:36'),
+(102, 18, 1, 4, '2023-12-23 18:51:11'),
+(103, 18, 3, 10, '2023-12-23 18:51:17'),
+(104, 18, 4, 14, '2023-12-23 18:51:24'),
+(105, 18, 5, 16, '2023-12-23 18:51:30'),
+(106, 18, 6, 19, '2023-12-23 18:51:35'),
+(107, 18, 7, 22, '2023-12-23 18:51:46'),
+(108, 18, 8, 25, '2023-12-23 18:51:55'),
+(109, 18, 9, 30, '2023-12-23 18:52:05'),
+(110, 18, 10, 33, '2023-12-23 18:52:15'),
+(111, 18, 11, 38, '2023-12-23 18:52:22'),
+(112, 18, 12, 42, '2023-12-23 18:52:29'),
+(113, 19, 1, 4, '2023-12-23 18:53:58'),
+(114, 19, 3, 11, '2023-12-23 18:54:07'),
+(115, 19, 4, 13, '2023-12-23 18:54:14'),
+(116, 19, 5, 15, '2023-12-23 18:54:20'),
+(117, 19, 6, 18, '2023-12-23 18:54:28'),
+(118, 19, 7, 22, '2023-12-23 18:54:42'),
+(119, 19, 8, 25, '2023-12-23 18:54:49'),
+(120, 19, 9, 31, '2023-12-23 18:54:58'),
+(121, 19, 10, 34, '2023-12-23 18:55:08'),
+(122, 19, 11, 37, '2023-12-23 18:55:18'),
+(123, 19, 12, 40, '2023-12-23 18:55:29'),
+(124, 20, 1, 1, '2023-12-23 19:51:52'),
+(125, 20, 3, 10, '2023-12-23 19:52:32'),
+(126, 20, 6, 19, '2023-12-23 19:52:50'),
+(127, 20, 12, 40, '2023-12-23 19:53:07'),
+(128, 6, 5, 16, '2023-12-24 08:15:46'),
+(129, 6, 12, 43, '2023-12-24 08:16:00'),
+(131, 8, 14, 47, '2023-12-24 08:19:52'),
+(133, 1, 14, 48, '2023-12-24 09:20:33'),
+(134, 1, 8, 28, '2023-12-24 09:20:46'),
+(135, 1, 12, 43, '2023-12-24 09:21:36'),
+(136, 1, 9, 31, '2023-12-24 09:22:45');
 
 --
 -- Indexes for dumped tables
@@ -198,9 +331,9 @@ ALTER TABLE `polls`
 --
 -- Indexes for table `users`
 --
-  ALTER TABLE `users`
-    ADD PRIMARY KEY (`id`),
-    ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `votes`
@@ -219,25 +352,25 @@ ALTER TABLE `votes`
 -- AUTO_INCREMENT for table `options`
 --
 ALTER TABLE `options`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `polls`
 --
 ALTER TABLE `polls`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
 
 --
 -- Constraints for dumped tables
