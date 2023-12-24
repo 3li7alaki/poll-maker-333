7b0b5dbe863b7f2f1b10e2b0ca9374a9c8d2a5c5
Ali AlHalaki <AliHalaki@outlook.com>
2023-12-24 12:53:16 +0300

    Fix

--- pollmaker.sql
+++ pollmaker.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `u515027391_pollmaker`
+-- Database: `pollmaker`
 --
 
 -- --------------------------------------------------------
